f891f92c6b2a15b0785d93059f29efb7500afbf2
Nicolas Chambrier <naholyr@gmail.com>
2012-03-16 13:28:59 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+hello(); // ← use before declaration
+
+var bar; // ← unused variable
+
+function hello() {
+  x = 1; // ← undeclared variable
+  return true // ← missing semi-colon
+}
+
+var foo = function () {
+} // ← missing semi-colon
+
+hello();
+
+console.log("end of script");
